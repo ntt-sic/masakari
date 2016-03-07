6f34d78ff7d2adf7d00776ecfe4e71e13ebba87c
sampathP <sam47priya@gmail.com>
2016-03-07 16:47:20 +0900

    Relocate the controller/db

--- masakari-controller/db/create_database.sh
+++ masakari-controller/db/create_database.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
-#
-#
+#!/bin/bash
 
-source db.conf
-
-mysql -u${DB_USER} -p${DB_PASSWORD} -h${DB_HOST} -e "drop database vm_ha"
-mysql -u${DB_USER} -p${DB_PASSWORD} -h${DB_HOST} -e "source create_vmha_database.sql"
+# directory where this script exists
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+cd ../..
+python -m masakari-controller.db.create_tables
--- masakari-controller/db/db/create_database.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# directory where this script exists
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-cd ../..
-python -m masakari-controller.db.create_tables
